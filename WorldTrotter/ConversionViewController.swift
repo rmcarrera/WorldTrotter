a1d8529df0b8f19d3e960dae5014dc9bb477b5c4
rmcarrera <rcarrera@highpoint.edu>
2017-02-03 13:59:52 -0500

    Added another view, a map view.

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -51,6 +51,8 @@ class ConversionViewController : UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("ConversionViewController opened its view")
+        
         updateCelsiusLabel()
     }
     
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Roxana Carrera on 2/3/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController did load")
+    }
+}
