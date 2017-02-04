db52b5b2fe2f1ad7e198a66e2531180612a391b4
rmcarrera <rcarrera@highpoint.edu>
2017-02-04 00:58:37 -0500

    Added the ability to change map type in the map view.

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,8 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController{
+    var mapView: MKMapView!
+    
+    func mapTypeChanged(_ segControl: UISegmentedControl){
+        switch segControl.selectedSegmentIndex{
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
+    override func loadView() {
+        //create a map view
+        mapView = MKMapView()
+        
+        //set it as the view of this view controller
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstriant = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingConstriant = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstriant = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstriant.isActive = true
+        leadingConstriant.isActive = true
+        trailingConstriant.isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController did load")
